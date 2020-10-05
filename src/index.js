705faffe927c4108b3aa477c34a478ac470b2800
Huriosity <vetani3123@mail.ru>
2020-10-05 20:28:59 +0300

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        return (n*-1).toString().split('').reverse().join('');
+    }
+
+    return (n).toString().split('').reverse().join('');
 }
